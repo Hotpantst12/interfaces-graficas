bab178a37a69e4a3904bb48c0224bf45810f028a
Hotpantst12 <94929515+Hotpantst12@users.noreply.github.com>
2023-12-01 11:14:13 -0500

    some changes
    centered the text, added some frames and yeah

--- test1.py
+++ test1.py
@@ -120,13 +120,39 @@ class Aplicacion:
         factura += f"\nTotal productos: {total_productos}€"
         factura += f"\nTotal a pagar: {total_servicios + total_productos}€"
 
-        # Crear una nueva ventana para mostrar la factura
+    # Crear una nueva ventana para mostrar la factura
         factura_ventana = tk.Toplevel(self.ventana)
         factura_ventana.title("Factura")
         factura_ventana.configure(bg="lightyellow")
-        factura_text = tk.Text(factura_ventana, bg="lightyellow", fg="black")
-        factura_text.pack()
-        factura_text.insert(tk.END, factura)
+
+    # Añadir un marco para el título de la factura
+        titulo_factura = tk.Label(factura_ventana, text="Factura", bg="lightblue", fg="black", font=("Helvetica", 16, "bold"))
+        titulo_factura.grid(row=0, column=1)
+
+    # Añadir un marco para el contenido de la factura
+        contenido_factura = tk.Text(factura_ventana, bg="lightyellow", fg="black")
+        contenido_factura.grid(row=1, column=1)
+        contenido_factura.insert(tk.END, factura)
+
+    # Centrar el texto en el widget Text
+        contenido_factura.tag_configure("center", justify="center", spacing1=4)
+        contenido_factura.tag_add("center", 1.0, "end")
+
+    # Añadir marcos a los lados de la ventana de la factura
+        factura_ventana.grid_rowconfigure(0, weight=1) # Hacer que la fila 0 se expanda para llenar la ventana
+        factura_ventana.grid_columnconfigure(0, weight=1) # Hacer que la columna 0 se expanda para llenar la ventana
+
+    # Crear los marcos laterales
+        marco_izquierdo = tk.Frame(factura_ventana, width=5, bg="black")
+        marco_izquierdo.grid(row=0, column=0, rowspan=2, sticky="ns")
+
+        marco_derecho = tk.Frame(factura_ventana, width=5, bg="black")
+        marco_derecho.grid(row=0, column=2, rowspan=2, sticky="ns")
+    # Crear los marco inferior
+        marco_inferior = tk.Frame(factura_ventana, height=5, bg="black")
+        marco_inferior.grid(row=2, column=0, columnspan=3, sticky="ew")
+
+
     def crear_cliente(self):
         id_cliente = self.id_entry.get()
         nombre_cliente = self.nombre_entry.get()
